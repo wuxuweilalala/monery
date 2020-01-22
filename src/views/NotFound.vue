63514422722b8f7cf7a7d4f9adebe14c5cab1519
wuxuweilalala <2509165479@qq.com>
2020-01-22 18:18:10 +0800

    添加404页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>当前网页不存在，请检查网址是否正确</div>
+    <div>
+      <router-link to="/">返回首页</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
