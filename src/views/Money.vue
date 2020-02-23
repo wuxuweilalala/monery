f5effe809d3586cbd884b4e8d6bd02e606c8bd5b
wuxuweilalala <2509165479@qq.com>
2020-02-23 22:16:58 +0800

    完成 Tags 组件

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -3,18 +3,23 @@
     <NumberPad />
     <Types :prop-a="33"/>
     <Notes />
-    <Tags />
+    <Tags :data-source.sync="tags"/>
   </Layout>
 </template>
 
-<script lang="ts">
+<script lang="js">
 import NumberPad from "@/components/Money/NumberPad.vue";
 import Types from "@/components/Money/Types.vue";
 import Notes from "@/components/Money/Notes.vue";
 import Tags from "@/components/Money/Tags.vue";
 export default {
   name: "Money",
-  components: { Tags, Notes, Types, NumberPad }
+  components: { Tags, Notes, Types, NumberPad },
+  data(){
+    return {
+      tags:['衣','食','住','行']
+    }
+  }
 };
 </script>
 
