b6ac84dec3c3970e9dfc95d9613eb359e85625c0
wuxuweilalala <2509165479@qq.com>
2020-03-18 22:14:43 +0800

    money.vue 使用 vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,27 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import clone from '@/lib/clone';
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
+const store =  new Vuex.Store({
   state: {
+    recordList:[] as RecordItem[],
   },
   mutations: {
+    createRecord(state,record:RecordItem){
+      const deepClone:RecordItem =  clone(record) ;
+      deepClone.createdAt = new Date();
+      state.recordList.push(deepClone);
+      store.commit('saveRecords')
+    },
+    saveRecords(state){
+      window.localStorage.setItem('recordList',JSON.stringify(state.recordList));
+    },
   },
   actions: {
   },
   modules: {
   }
 })
+export default store
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -16,7 +16,6 @@ import Types from "@/components/Money/Types.vue";
 import Notes from "@/components/Money/Notes.vue";
 import Tags from "@/components/Money/Tags.vue";
   import {Component} from 'vue-property-decorator';
-  import store from '@/store/index2';
 
 @Component({
   components: { Tags, Notes, Types, NumberPad },
@@ -24,7 +23,7 @@ import Tags from "@/components/Money/Tags.vue";
 export default class Money extends Vue{
   record: RecordItem = {tags:[],notes:'',type:'-',amount:0}
   saveRecord(){
-    store.createRecord(this.record);
+    this.$store.commit('createRecord',this.record);
   }
 }
 </script>
