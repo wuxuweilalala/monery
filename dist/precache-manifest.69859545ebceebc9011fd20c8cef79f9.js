b2f0c8b561a14e0a82bd8af9c4dccca26ae4932f
wuxuweilalala <2509165479@qq.com>
2020-03-29 19:14:54 +0800

    添加 dist

---
+++ dist/precache-manifest.69859545ebceebc9011fd20c8cef79f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "839169e78e73c3430645",
+    "url": "/css/app.56568d66.css"
+  },
+  {
+    "revision": "42facb1a1a72fc336325d33de6427776",
+    "url": "/index.html"
+  },
+  {
+    "revision": "839169e78e73c3430645",
+    "url": "/js/app.542bd703.js"
+  },
+  {
+    "revision": "44fb37bd140603f50b78",
+    "url": "/js/chunk-vendors.717346be.js"
+  },
+  {
+    "revision": "f0fce8a1d94de3ce6d9839c05c88d85e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
